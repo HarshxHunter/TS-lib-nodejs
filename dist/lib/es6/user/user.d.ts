568140387df58fac3bf11187484d02983390b3ce
Harsh Singh Kaira <kairaharsh793@gmail.com>
2023-11-21 15:08:13 +0530

    gitignore 3 attempt

--- dist/lib/es6/user/user.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class PlayerStats {
-    apps: number;
-    goals: number;
-    wins: number;
-}
--- dist/lib/es6/user/user.js
+++
@@ -1,9 +0,0 @@
-var PlayerStats = /** @class */ (function () {
-    function PlayerStats() {
-        this.apps = 0;
-        this.goals = 0;
-        this.wins = 0;
-    }
-    return PlayerStats;
-}());
-export { PlayerStats };
